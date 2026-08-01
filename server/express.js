5dd5e7645638487e70eb267b4f61e853f2bd0c38
rsonas <134102387+rsonas@users.noreply.github.com>
2026-08-01 19:30:02 -0400

    updated cors

--- server/express.js
+++ server/express.js
@@ -5,11 +5,14 @@ import morgan from 'morgan'
 const app = express()
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://profile-complete.onrender.com/"
-    ], credentials: true
+    origin: "https://profile-complete-1.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
+
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
